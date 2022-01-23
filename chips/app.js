8a3a282e99e724723f6077b78dae594e4bbf66d6
Naga Satya Geeth <67518197+Artistole@users.noreply.github.com>
2022-01-23 12:31:38 +0530

    refac

---
+++ chips/app.js
@@ -0,0 +1,14 @@
+const autoInput = document.getElementById('auto-input');
+
+// autoInput.addEventListener('keydown', newchip);
+
+// function newchip()
+// {
+//     // if(newchip.key === 'Enter')
+//     console.log(newchip.key)  
+        
+// }
+
+autoInput.addEventListener("keydown", (e) => {
+    console.log(e.key);
+})
