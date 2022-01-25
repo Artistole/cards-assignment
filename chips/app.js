df30110b67fca7b4db1b16ba3e355b30446c7a48
Naga Satya Geeth <67518197+Artistole@users.noreply.github.com>
2022-01-25 14:39:48 +0530

    refactor

--- chips/app.js
+++ chips/app.js
@@ -1,5 +1,7 @@
 const autoInput = document.getElementById('auto-input');
 
+const output = document.getElementById('output')
+
 // autoInput.addEventListener('keydown', newchip);
 
 // function newchip()
@@ -9,6 +11,23 @@ const autoInput = document.getElementById('auto-input');
         
 // }
 
-autoInput.addEventListener("keydown", (e) => {
-    console.log(e.key);
-})
+//cant use just a variable we have to pass an
+autoInput.addEventListener('keydown', e=> newchip(e) );
+
+function newchip(e)
+{
+    var chipinput = autoInput.value
+    if(e.key === 'Enter' && chipinput !== "")
+    {
+       output.innerHTML += `<div class="chips-cont">
+       ${chipinput}
+       <button class="cross-btn" onclick='remove()'><i class="fas fa-times-circle"></i></button>
+       </div>`
+       document.getElementById('auto-input').value=''
+    }
+}
+
+function remove(){
+    var crossBtn = document.querySelector('.chips-cont');
+    crossBtn.remove();
+}
